e02294e669b5c66f8a7cf5c4db858394883181dd
Sarabjeet Singh <sarabsingh212@gmail.com>
2025-03-07 21:13:49 +0530

    added responsive flavours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start container w-[900px]">
-        <div className="flex gap-4 bg-amber-100 flex-col p-4 w-[300px] rounded-xl">
+      <main className="flex flex-col sm:flex-row gap-8 row-start-2 items-center sm:items-start container lg:w-[900px]">
+        <div className="flex gap-4 bg-amber-100 flex-col p-4 lg:w-[300px] rounded-xl items-center lg:items-start">
           <Image src="/sarabjeet-400.JPG" alt="Sarabjeet Singh" width={200} height={200} className="rounded-xl" />
           <h1 className="text-black text-2xl font-bold">Sarabjeet Singh</h1>
           <h2 className="text-black">A Software Engineer who has developed countless innovative solutions.</h2>
