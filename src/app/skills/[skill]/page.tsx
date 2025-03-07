ef1f0c622e4963bbc73128aee47f3862dd06fb6f
Sarabjeet Singh <sarabsingh212@gmail.com>
2025-03-07 20:58:44 +0530

    Dynamic Skill page

---
+++ src/app/skills/[skill]/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import Nav from "@/components/nav";
+import { useParams } from "next/navigation";
+
+export default function Skill() {
+    const params = useParams();
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <Nav />
+    <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start container w-[900px]">
+        <div className="flex gap-4 flex-col p-4 w-[300px] rounded-xl">
+        <h1 className="text-gray-400 text-6xl uppercase tracking-widest">{params?.skill}</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
