f69a12a3656fe4240d5304db17dae8fcdb5197a3
Sarabjeet Singh <sarabsingh212@gmail.com>
2025-03-11 21:59:52 +0530

    Quotes page and data

---
+++ src/app/quotes/data.ts
@@ -0,0 +1,15 @@
+export const quotes = [{
+    content: `Hiring great engineers is not enough.
+
+You can have the smartest team in the world, but if they don't collaborate well, you're stuck.
+
+If they can't communicate their ideas, projects will drag on.
+If they don't care about the business, they'll build tech for the sake of tech.
+A great engineering team isn't just about code quality.
+
+It's about trust, ownership, and understanding the bigger picture.
+
+Tech is easy. People are hard.`,
+id: 1,
+link: "https://www.linkedin.com/posts/alessiomavica_one-of-the-hardest-lessons-i-learned-as-a-activity-7302296731822575616-ao0n"
+}];
---
+++ src/app/quotes/page.tsx
@@ -0,0 +1,23 @@
+import Nav from "@/components/nav";
+import { quotes } from "./data";
+
+export default function Qquotes() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <Nav />
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start container w-[900px]">
+        <div className="flex gap-4 flex-col p-4 rounded-xl">
+        <h1 className="text-gray-400 text-6xl uppercase tracking-widest">Quotes</h1>
+        <div className="flex w-full flex-col">
+            {quotes.map((quote) => (
+                <div key={quote.id} className="bg-orange-50 text-black rounded-2xl p-4 w-full">
+                    <pre>{quote.content}</pre>
+                    <a className="text-sm text-sky-300 underline" target="_blank" href={quote.link}>Original Post</a>
+                </div>
+            ))}
+        </div>
+        </div>
+      </main>
+    </div>
+  );
+}
